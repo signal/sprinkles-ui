027be3dd672a336d24b031086e60d202949161c5
Harrison Harnisch <hharnisch@signal.co>
2016-01-13 15:43:39 -0600

    Create sample component

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+/**
+ * Dummy component.For reference
+ */
+export default class MyComponent extends React.Component {
+  render() {
+    return (
+      <div style={{
+          background: this.props.color,
+          color: "white",
+          padding: 40 }}>
+        MyComponent
+      </div>
+    );
+  }
+}
+
+
+// API --------------------------------------------------------
+MyComponent.propTypes = {
+  color: React.PropTypes.oneOf(["red", "green", "blue"]),
+};
+MyComponent.defaultProps = {
+  color: "blue"
+};
---
+++ src/specs/index.js
@@ -0,0 +1,15 @@
+/*
+  The index of specs.
+
+  Notice the require statement is embedded within a "describe"
+  block.  Use this pattern to structure, nest and organize your
+  visual tests.
+
+*/
+
+
+describe("Quick Start (Sample)", function() {
+  require("./MyComponent.spec");
+  require("./MenuItem.spec");
+  require("./Menu.spec");
+});
