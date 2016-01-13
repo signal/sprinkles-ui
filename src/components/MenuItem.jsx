5b050d3e7169831e80061f7fe3f3eef154244d6c
Harrison Harnisch <hharnisch@signal.co>
2016-01-13 15:45:06 -0600

    Add MenuItem

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class MenuItem extends React.Component {
+
+  static propTypes = {
+    handleClick: React.PropTypes.func,
+    text: React.PropTypes.string,
+    style: React.PropTypes.object
+  };
+
+  static defaultProps = {
+    text: "",
+    style: {}
+  };
+
+  handleClick = () => {
+    if (this.props.handleClick) {
+      this.props.handleClick()
+    }
+  };
+
+  render () {
+    return (
+      <li style={this.props.style} onClick={ this.handleClick }>
+        {this.props.text}
+      </li>
+    );
+  }
+}
---
+++ src/specs/MenuItem.spec.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import loremIpsum from "lorem-ipsum";
+import MenuItem from "../components/MenuItem";
+
+
+describe("MenuItem", function() {
+  this.header(`## MenuItem`); // Markdown.
+
+  before(() => {
+    function handleClick() {
+      console.log('click!');
+    }
+
+    // Runs when the Suite loads.  Use this to host your component-under-test.
+    this.load( <MenuItem
+        color="blue"
+        text={ loremIpsum() }
+        handleClick={ handleClick }
+      />
+    );
+  });
+
+  it("Update Text", () => this.props({ text: loremIpsum() }));
+
+  it("Style 1", () => this.props({ style: {}}));
+  it("Style 2", () => this.props({ style: {background: "#4183F0", color: "white"}}));
+
+  /**
+   * Documentation (Markdown)
+   */
+  this.footer(`
+  ### MenuItem
+  `);
+});
