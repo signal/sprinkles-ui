f9b4d82db652ed0e72b4b9ee44c4736b82a8c3f1
Harrison Harnisch <hharnisch@signal.co>
2016-01-25 09:14:07 -0600

    Added test to ensure that click events are raised when ListItems are clicked in a rendered List

--- __tests__/List.test.jsx
+++ __tests__/List.test.jsx
@@ -9,9 +9,8 @@ import TestUtils from "react-addons-test-utils";
 // TODO: move this to es6 style import when its implemented in jest
 const List = require("../src/components/List").default;
 
-
 describe("List", () => {
-  it("Renders a List", () => {
+  it("Does render a List", () => {
     const listItems = [
       {
         key: "1",
@@ -25,14 +24,53 @@ describe("List", () => {
       }
     ];
     // Render a List with no style
-    const menu = TestUtils.renderIntoDocument(
+    const list = TestUtils.renderIntoDocument(
         <List listItems={listItems} />
     );
 
     // grab the DOM node so we can inspect it
-    TestUtils.scryRenderedDOMComponentsWithClass(menu, "li").map((item,i) => {
+    TestUtils.scryRenderedDOMComponentsWithTag(list, "li").forEach((item,i) => {
       expect(item.textContent).toEqual(listItems[i].text);
     });
   });
 
+  it("Does trigger an event when list item clicked", (done) => {
+    const listItems = [
+      {
+        key: "1",
+        text: "Item 1",
+        selected: false
+      },
+      {
+        key: "2",
+        text: "Item 2",
+        selected: true
+      }
+    ];
+
+    // create a copy of the list items
+    let setItems = new Set(listItems);
+
+    // handle click events
+    function clickEvent(item) {
+      setItems.delete(item);
+
+      // all items have been clicked
+      if (setItems.size === 0) {
+        done();
+      }
+    }
+
+    // Render a List with no style
+    const list = TestUtils.renderIntoDocument(
+        <List
+            handleItemClick={clickEvent}
+            listItems={listItems}
+        />
+    );
+
+    TestUtils.scryRenderedDOMComponentsWithTag(list, "li").forEach((item) => {
+      TestUtils.Simulate.click(item);
+    });
+  });
 });
