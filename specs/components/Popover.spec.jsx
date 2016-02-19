06aadee36bee5113656c72c4e11c869862ef42cf
Harrison Harnisch <hharnisch@signal.co>
2016-02-19 10:23:05 -0600

    Wrap Popover for testing

--- specs/components/Popover.spec.jsx
+++ specs/components/Popover.spec.jsx
@@ -3,20 +3,71 @@ import loremIpsum from "lorem-ipsum";
 import Popover from "../../src/components/Popover";
 
 
+// Wrapper to contain a Popover and an anchor element
+class PopoverWrapper extends React.Component {
+  displayName = "PopoverWrapper";
+
+  static propTypes = {
+      open: React.PropTypes.bool,
+      text: React.PropTypes.string
+  };
+
+  static defaultProps = {
+    open: false,
+    text: ""
+  };
+
+  constructor () {
+    super();
+
+    this.style = {
+      anchorDiv: {
+        padding: 10,
+        background: "#4285F4",
+        color: "#FEFEFE"
+      }
+    }
+  }
+
+  handleClick (e) {
+    console.log("click");
+  }
+
+  render () {
+    return(
+        <div>
+            <div
+                onClick={this.handleClick}
+                style={this.style.anchorDiv}
+            >
+                {"Click Me"}
+            </div>
+            <Popover
+                open={this.props.open}
+            >
+                {this.props.text}
+            </Popover>
+        </div>
+    );
+  }
+}
+
 describe("Popover", function() {
   this.header(`## Popover`); // Markdown.
 
   before(() => {
     // Runs when the Suite loads.  Use this to host your component-under-test.
     this.load(
-        <Popover open={true}>{loremIpsum()}</Popover>
+        <PopoverWrapper
+            text={loremIpsum()}
+        />
     );
   });
 
   it("Close Popver", () => this.props({open: false}));
   it("Open Popver", () => this.props({open: true}));
 
-    /**
+  /**
    * Documentation (Markdown)
    */
   this.footer(`
