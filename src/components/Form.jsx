78316c399f1332747a7f9397f746e9fb3738da55
Harrison Harnisch <hharnisch@signal.co>
2016-03-11 09:44:51 -0600

    Stub in click handler and form validation funcitons

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,10 +5,30 @@ import Button from "./Button"
 export default class Form extends ReactCSS.Component {
   displayName = "Form";
 
+  static propTypes = {
+    children: React.PropTypes.node,
+    onSubmit: React.PropTypes.func
+  };
+
+  static defaultProps = {
+    onSubmit: () => {}
+  };
+
+  handleClick (e) {
+    if (this.isValid()) {
+      this.props.onSubmit();
+    }
+  }
+
+  isValid () {
+    return true;
+  }
+
   render () {
     return(
         <div>
             <Button
+                onClick={this.handleClick.bind(this)}
                 ref={c => this.submitButtonRef = c}
             />
         </div>
