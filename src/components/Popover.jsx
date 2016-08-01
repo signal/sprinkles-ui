6a0c35ccb6481a540d22f16a975555ea0dcc0e31
Ryan Balla <rballa@signal.co>
2016-08-01 13:04:23 -0500

    Adding position to hidden close layer to prevent UI jumping

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -109,6 +109,7 @@ export default class Popover extends React.Component {
           flexFlow: 'column nowrap',
         },
         CloseLayer: {
+          position: 'absolute',
           visibility: 'hidden',
         },
       },
