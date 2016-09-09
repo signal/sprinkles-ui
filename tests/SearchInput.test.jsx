d916523ed3fdfb1523d2e176e202f67ac0b1a826
Ryan Balla <rballa@signal.co>
2016-09-09 16:44:10 -0500

    Adding tests to search input

--- tests/SearchInput.test.jsx
+++ tests/SearchInput.test.jsx
@@ -6,6 +6,20 @@ import SearchInput from '../src/components/SearchInput';
 describe('SearchInput', () => {
   let emptySearchInputComponent;
   let emptySearchInputNode;
+  const handleChange = jest.genMockFunction();
+  const record = [{
+    title: 'Old Man\'s War',
+    author: {
+      firstName: 'John',
+      lastName: 'Scalzi',
+    },
+  }];
+  const searchSettings = {
+    keys: [
+      'title',
+      'author.firstName',
+    ],
+  };
 
   beforeEach(() => {
     emptySearchInputComponent = TestUtils.renderIntoDocument(
@@ -33,12 +47,6 @@ describe('SearchInput', () => {
     expect(searchInputNode.disabled).toBe(true);
   });
   it('Does merge search settings with defaults', () => {
-    const searchSettings = {
-      keys: [
-        'title',
-        'author.firstName',
-      ],
-    };
     const searchInputComponent = TestUtils.renderIntoDocument(
       <SearchInput
         searchableRecords={[]}
@@ -49,20 +57,6 @@ describe('SearchInput', () => {
     expect(searchInputComponent.state.combinedSearchSettings.keys).toEqual(searchSettings.keys);
   });
   it('Does trigger a change event on search', () => {
-    const handleChange = jest.genMockFunction();
-    const record = [{
-      title: 'Old Man\'s War',
-      author: {
-        firstName: 'John',
-        lastName: 'Scalzi',
-      },
-    }];
-    const searchSettings = {
-      keys: [
-        'title',
-        'author.firstName',
-      ],
-    };
     const searchInputComponent = TestUtils.renderIntoDocument(
       <SearchInput
         onChange={handleChange}
@@ -78,6 +72,21 @@ describe('SearchInput', () => {
       value: 'zoo',
       searchResults: [],
     });
+  });
+
+  it('Returns search results', () => {
+    const searchInputComponent = TestUtils.renderIntoDocument(
+      <SearchInput
+        onChange={handleChange}
+        searchableRecords={record}
+        searchSettings={searchSettings}
+      />
+    );
+    const searchInputNode = ReactDOM.findDOMNode(searchInputComponent.inputRef);
+    searchInputNode.value = 'Old';
+    TestUtils.Simulate.change(searchInputNode);
+
+    expect(searchInputComponent.state.searchResults).toEqual(record);
   });
 
   it('Does show a clear search box option when value is entered', () => {
@@ -99,5 +108,24 @@ describe('SearchInput', () => {
     expect(emptySearchInputComponent.state.value).toEqual('');
     expect(emptySearchInputComponent.state.isFocused).toEqual(false);
     expect(clearSearchNode.style.visibility).toEqual('hidden');
+  });
+
+  it('Does clear search results on clear state', () => {
+    const searchInputComponent = TestUtils.renderIntoDocument(
+      <SearchInput
+        onChange={handleChange}
+        searchableRecords={record}
+        searchSettings={searchSettings}
+      />
+    );
+
+    const searchInputNode = ReactDOM.findDOMNode(searchInputComponent.inputRef);
+    searchInputNode.value = 'Old';
+    TestUtils.Simulate.change(searchInputNode);
+
+    const clearSearchNode = ReactDOM.findDOMNode(searchInputComponent.clearSearchRef);
+    TestUtils.Simulate.click(clearSearchNode);
+
+    expect(searchInputComponent.state.searchResults).toEqual([]);
   });
 });
